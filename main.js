abacae883bfb9f111477b7fc8dfb8c2670ee4490
sean-code <ngangajohn536@gmail.com>
2023-11-24 17:41:16 +0300

    Create: Error Catcher

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const jobCategoryRoutes = require('./routes/jobCategoryRoutes')
 const applicationRoutes = require('./routes/applicationRoutes');
 const applicantRoutes = require('./routes/applicantRoutes');
 
-
 // other route imports...
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!");
+});
 
 app.use('/api', jobRoutes);
 app.use('/api', jobCategoryRoutes);
